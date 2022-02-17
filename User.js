c4e0ecbade967b8cc85b1b70b8fdde3dd35f0eb7
UmamaheshMaxwell <maheshmeka16@gmail.com>
2022-02-17 13:31:28 +0530

    added APIs using mongoDB and SQL Serer

--- User.js
+++ User.js
@@ -1,42 +1,79 @@
 const express = require("express")
 const app = express()
 const cors = require('cors')
+const mongoose = require("mongoose")
+const User = require("./models/userModels")
+const router = express.Router()
+
+
+mongoose.connect("mongodb://localhost:27017/userdb", (err) => {
+    if(err){
+        throw err
+    } else {
+        console.log(`Connected to mongodb successfully`)
+    }
+})
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.json("Welcome to User API")
 })
 
-app.get("/users", (req, res) => {
-    const users = [
-        {
-            id: 1,
-            name: 'Scott',
-            age:45,
-            skill: 'React'
-        },
-        {
-            id: 2,
-            name: 'Adam',
-            age:44,
-            skill: 'Angular'
-        },
-        {
-            id: 3,
-            name: 'Tuan',
-            age:42,
-            skill: 'VueJS'
-        },
-        {
-            id: 4,
-            name: 'Uma',
-            age:39,
-            skill: 'Javascript'
-        },
-    ]
-    res.json(users)
+router.get("/users", (req, res) => {
+    User.getUsers(function(err, data){
+        if(err){
+            throw err
+        }
+        res.json(data)
+    })    
 })
+
+router.get("/users/:id", (req, res)=>{
+    const userId = req.params.id
+    User.getUserById(userId, (err, data) => {
+        if(err){
+            throw err
+        }
+        res.json(data)
+    })
+})
+
+router.post("/users", (req, res)=>{
+    const user = req.body
+    User.createUser(user, (err, data) =>{
+        if(err){
+            throw err
+        }
+        res.json(data)
+    })
+})
+
+router.put("/users/:id", (req, res) => {
+    const userId = req.params.id
+    const user = req.body
+    User.updateUser(userId, user, (err, data) =>{
+        if(err){
+            throw err
+        }
+        res.json(data)
+    })
+})
+
+router.delete("/users/:id", (req, res)=>{
+    const userId = req.params.id
+
+    User.deleteUser(userId, (err, data) =>{
+        if(err){
+            throw err
+        }
+        res.json(data)
+    })
+})
+
+app.use("/api", router)
 
 const PORT = 3001
 
